6b9e0f96fad1026229b66df9515e4cfccd9525ad
TruongHQ <>
2024-11-21 00:22:17 +0700

    + Fist commit: Add project.

---
+++ AnimalRacing/AnimalRacing/AnimalRacingApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimalRacingApp.swift
+//  AnimalRacing
+//
+//  Created by Trường  on 11/21/24.
+//
+
+import SwiftUI
+
+@main
+struct AnimalRacingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
